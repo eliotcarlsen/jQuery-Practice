21fd4a6f0d5cde9c54ac59cd35b5c806417f9cb4
Eliot Carlsen and Max Larson <eliot@>
2017-02-14 11:56:29 -0800

    add flashcard game to our jQuery learning

---
+++ scripts.js
@@ -0,0 +1,29 @@
+$(function(){
+  $(".clickable").click(function() {
+    $("#ball").slideToggle();
+  });
+  $("#topA").click(function() {
+    $("#cardA").toggle();
+  });
+  $("#topB").click(function() {
+    $("#cardB").toggle("#showCard");
+  });
+  $("#topC").click(function() {
+    $("#cardC").toggle("#showCard");
+  });
+  $("#topD").click(function() {
+    $("#cardD").toggle("#showCard");
+  });
+  $("#topE").click(function() {
+    $("#cardE").toggle("#showCard");
+  });
+  $("#topF").click(function() {
+    $("#cardF").toggle("#showCard");
+  });
+  $("#topG").click(function() {
+    $("#cardG").toggle("#showCard");
+  });
+  $("#topH").click(function() {
+    $("#cardH").toggle("#showCard");
+  });
+  });
